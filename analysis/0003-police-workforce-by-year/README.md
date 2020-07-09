bb615e4a21e354114939c47b4a6eba65aaeb44aa
WheresAlice <WheresAlice@users.noreply.github.com>
2020-07-09 09:31:02 +0100

    police workforce count by year, in a single file

---
+++ analysis/0003-police-workforce-by-year/README.md
@@ -0,0 +1,9 @@
+A single CSV containing FTE workforce count for police forces in England and Wales
+
+[police-workforce-fte-by-year.csv](police-workforce-fte-by-year.csv)
+
+Some years have different rows - some include non-geographical forces and totals
+
+We use the March numbers to better align to other data which uses financial year
+
+No test, because the only meaningful test is baked into the python script, asserting that the length of output is equal to the length of the inputs
---
+++ analysis/0003-police-workforce-by-year/workforce.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import os
+
+fte_root = os.path.join(os.path.dirname(__file__), '..', '..', 'cleaned_data', 'police-population', 'officer-fte')
+
+fte_15 = pd.read_csv(os.path.join(fte_root, '2015.csv'))
+fte_16 = pd.read_csv(os.path.join(fte_root, '2016.csv'))
+fte_17 = pd.read_csv(os.path.join(fte_root, '2017.csv'))
+fte_18 = pd.read_csv(os.path.join(fte_root, '2018.csv'))
+fte_19 = pd.read_csv(os.path.join(fte_root, '2019.csv'))
+
+fte_15.drop(["September 2014", "September 2015"], axis=1, inplace=True)
+fte_15['year'] = "2015"
+fte_15.columns = ["Force/Region", "FTE Count", "Year"]
+
+fte_16.drop(["September 2015", "September 2016"], axis=1, inplace=True)
+fte_16['year'] = "2016"
+fte_16.columns = ["Force/Region", "FTE Count", "Year"]
+
+fte_17.drop(["September 2016", "September 2017"], axis=1, inplace=True)
+fte_17['year'] = "2017"
+fte_17.columns = ["Force/Region", "FTE Count", "Year"]
+
+fte_18.drop(["September 2017", "September 2018"], axis=1, inplace=True)
+fte_18['year'] = "2018"
+fte_18.columns = ["Force/Region", "FTE Count", "Year"]
+
+fte_19.drop(["September 2018", "September 2019"], axis=1, inplace=True)
+fte_19['year'] = "2019"
+fte_19.columns = ["Force/Region", "FTE Count", "Year"]
+
+output = pd.concat([fte_15, fte_16, fte_17, fte_18, fte_19])
+
+assert (len(fte_15) + len(fte_16) + len(fte_17) + len(fte_18) + len(fte_19) == len(output))
+
+output.to_csv("police-workforce-fte-by-year.csv", index=False)
