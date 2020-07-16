61816793e008c0a8856cff6fa6cdeba5458a8c19
WheresAlice <WheresAlice@users.noreply.github.com>
2020-07-16 19:17:32 +0100

    fix paths

--- analysis/0005-taser-use-by-ethnicity/taser-use-by-ethnicity.py
+++ analysis/0005-taser-use-by-ethnicity/taser-use-by-ethnicity.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import os
 
-repo_root = '/Users/kim/src/resistancelab_data'
+repo_root = os.path.join(os.path.dirname(__file__), '..', '..')
 df = pd.read_csv(
     os.path.join(repo_root, 'cleaned_data', 'use-of-force', 'by-ethnicity-force', 'april2018-march2019.csv'))
 df['ytd'] = '2019-03-31'
@@ -9,4 +9,4 @@ df = df.loc[df['Tactic'] == 'CED']
 df = df[df['Police Force'] != "Total England and Wales"]
 df = df.melt(id_vars=['Police Force', 'Tactic'], value_vars=['White', 'Black (or Black British)', 'Asian (or Asian British)', 'Chinese', 'Mixed', 'Other', "Don't know", 'Not reported'])
 
-df.to_csv("analysis/0005-taser-use-by-ethnicity/taser-use-by-ethnicity.csv", index=False)
+df.to_csv("taser-use-by-ethnicity.csv", index=False)
