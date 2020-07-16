6387ddc2cef2a7455e50395127da36ab4c61b88e
Dr Kim Foale <kim@gfsc.studio>
2020-07-16 17:37:01 +0100

    Fix previous commit which went weird

--- analysis/0005-taser-use-by-ethnicity/taser-use-by-ethnicity.py
+++ analysis/0005-taser-use-by-ethnicity/taser-use-by-ethnicity.py
@@ -8,21 +8,12 @@
 import pandas as pd
 import os
 
-repo_root = os.path.join(os.path.dirname(__file__), '..', '..')
-
-
-# These three years come from use-of-force data, and are all the same format
-april_2018_march_2019 = pd.read_csv(
+repo_root = '/Users/kim/src/resistancelab_data'
+df = pd.read_csv(
     os.path.join(repo_root, 'cleaned_data', 'use-of-force', 'by-ethnicity-force', 'april2018-march2019.csv'))
-april_2018_march_2019['ytd'] = '2019-03-31'
-april_2018_march_2019.drop(["Region"], axis=1, inplace=True)
+df['ytd'] = '2019-03-31'
+df = df.loc[df['Tactic'] == 'CED']
+df = df[df['Police Force'] != "Total England and Wales"]
+df = df.melt(id_vars=['Police Force', 'Tactic'], value_vars=['White', 'Black (or Black British)', 'Asian (or Asian British)', 'Chinese', 'Mixed', 'Other', "Don't know", 'Not reported'])
 
-
-# Concat all the data
-dataframes = [april_2018_march_2019, april_2017_march_2018, january_december_2016_revised, january_december2015_revised]
-output = pd.concat(dataframes)
-output = output[~output["Police force"].str.contains("Total")]
-output = output[~output["Police force"].isin(
-    ["England and Wales", "East of England", "South West", "South East", "London", "Wales", "Yorkshire and the Humber",
-     "North East", "North West"])]
-output.to_csv("taser-use-by-force-by-year.csv", index=False)
+df.to_csv("analysis/0005-taser-use-by-ethnicity/taser-use-by-ethnicity.csv", index=False)
