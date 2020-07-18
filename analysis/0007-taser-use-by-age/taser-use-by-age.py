d3eb4062bc83dcb97630a56aa44b44f5c86375a3
WheresAlice <WheresAlice@users.noreply.github.com>
2020-07-18 15:19:01 +0100

    resolve flake8 warnings

--- analysis/0007-taser-use-by-age/taser-use-by-age.py
+++ analysis/0007-taser-use-by-age/taser-use-by-age.py
@@ -1,16 +1,14 @@
 import pandas as pd
 import os
 
-repo_root = os.path.join("/Users/kim/src/resistancelab_data")
+repo_root = os.path.join(os.path.dirname(__file__), '..', '..')
 df = pd.read_csv(
     os.path.join(repo_root, 'cleaned_data', 'use-of-force', 'by-age-force', 'april2018-march2019.csv'))
 df['ytd'] = '2019-03-31'
 df = df.loc[df['Tactic'] == 'CED']
-df = df.drop(columns = 'Type of force')
+df = df.drop(columns='Type of force')
 df = df[df['Police Force'] != "Total England and Wales"]
-df.columns = [ 'Police Force', 'Tactic', '0-11', '11-17', '18-34', '35-49', '50-64', '65+', 'Not reported', 'Total', 'ytd' ]
-df
+df.columns = ['Police Force', 'Tactic', '0-11', '11-17', '18-34', '35-49', '50-64', '65+', 'Not reported', 'Total', 'ytd']
 
 melted = df.melt(id_vars=['Police Force', 'Tactic'], value_vars=['0-11', '11-17', '18-34', '35-49', '50-64', '65+', 'Not reported', 'Total'])
-melted
-melted.to_csv("analysis/0007-taser-use-by-age/taser-use-by-age.csv", index=False)
+melted.to_csv("taser-use-by-age.csv", index=False)
