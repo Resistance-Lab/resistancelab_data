3b048b21e466638fdfc1f3749610bc90ab009e47
WheresAlice <WheresAlice@users.noreply.github.com>
2021-02-23 18:05:23 +0000

    Fix missing 2020 data

--- analysis/0013-taser-use-by-age/taser-use-by-age.py
+++ analysis/0013-taser-use-by-age/taser-use-by-age.py
@@ -11,8 +11,7 @@ df_2020 = pd.read_csv(
     os.path.join(repo_root, 'cleaned_data', 'use-of-force', 'by-age-force', 'april2019-march2020.csv'))
 df_2020['ytd'] = '2020-03-31'
 
-df = pd.concat([df_2019, df_2020])
-df = df.loc[df['Tactic'] == 'CED']
+df = pd.concat([df_2019[df_2019['Tactic'] == 'CED'], df_2020[df_2020['Tactic'] == 'CED of which']])
 df = df.drop(columns='Type of force')
 df = df[df['Police Force'] != "Total England and Wales"]
 df.columns = ['Police Force', 'Tactic', '0-11', '11-17', '18-34', '35-49', '50-64', '65+', 'Not reported', 'Total', 'ytd']
